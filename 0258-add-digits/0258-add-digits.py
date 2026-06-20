95a05be1268a1723d2dc8137b0a3738787d3a4cd
arunima712 <arunimay712@gmail.com>
2026-06-20 14:18:15 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.62%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,10 +1,9 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        if num==0:
-            return 0
-        else:
-            return 1+(num-1)%9
-obj= Solution()
-print(obj.addDigits(38))
-
-    
+        if num < 10:
+            return num
+        digit_sum = 0
+        while num > 0:
+            digit_sum += num % 10
+            num //= 10
+        return self.addDigits(digit_sum)
