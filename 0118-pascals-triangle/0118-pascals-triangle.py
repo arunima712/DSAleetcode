6390676d00e04662a07974fef149cd7371c6f78b
arunima712 <arunimay712@gmail.com>
2026-06-21 14:09:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (29.25%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,11 +4,8 @@ class Solution:
         for i in range(numRows):
             row = []
             result = 1
-
             for j in range(1, i + 2):
                 row.append(result)
                 result = result * (i - j + 1) // j
-
             ans.append(row)
-
         return ans
